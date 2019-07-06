d33fefb900b1d39fe1baa15caea3a46d36e54dfa
jurentie <jurentie@gmail.com>
2019-07-06 14:39:46 -0600

    added basic symbol explanations to legend

--- poudre-dashboard-ng/src/app/map-components/map-layer-control/map-layer.component.ts
+++ poudre-dashboard-ng/src/app/map-components/map-layer-control/map-layer.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input }  from '@angular/core';
+import { Component, Input, OnInit }  from '@angular/core';
 
 @Component({
   selector: 'map-layer-component',
   styleUrls: ['./map-layer.component.css'],
   templateUrl:'./map-layer.component.html'
 })
-export class MapLayerComponent {
+export class MapLayerComponent implements OnInit{
     @Input() data: any;
+    @Input() layerView: any;
+    symbol: any = null;
+    ngOnInit(){
+      this.symbol = this.layerView.symbol;
+      console.log(this.symbol);
+    }
+
     toggleLayer() {
       this.data.mapReference.toggleLayer(this.data.geolayerId);
     }
