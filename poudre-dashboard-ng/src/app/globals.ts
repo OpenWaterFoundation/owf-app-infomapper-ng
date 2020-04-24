c9ebbd517320d35aa3b634faa3233bee3c4632b1
Nightsphere <nightsphere88@gmail.com>
2020-04-24 17:04:17 -0600

    Changed tab config file to app config file

--- poudre-dashboard-ng/src/app/globals.ts
+++ poudre-dashboard-ng/src/app/globals.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    configurationFile: string = "assets/map-configuration-files/map-tabs-configuration.json";
+    configurationFile: string = "assets/app-config.json";
 }
