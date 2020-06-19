5608690ec3ea99603d2b5034c0cd6c3af3dd9f44
Nightsphere <nightsphere88@gmail.com>
2020-06-19 11:44:49 -0600

    Updated the version numbers to be correct in the side panel

--- info-mapper/src/app/map-components/sidepanel-info/sidepanel-info.component.ts
+++ info-mapper/src/app/map-components/sidepanel-info/sidepanel-info.component.ts
@@ -1,10 +1,23 @@
-import { Component, Input }  from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { MapService }       from '../map.service';
 
 @Component({
-    selector: 'side-panel-info-component',
-    templateUrl: './sidepanel-info.component.html',
-    styleUrls: ['./sidepanel-info.component.css']
+  selector: 'side-panel-info-component',
+  templateUrl: './sidepanel-info.component.html',
+  styleUrls: ['./sidepanel-info.component.css']
 })
 export class SidePanelInfoComponent {
-    @Input() properties: any;
+  @Input() properties: any;
+  @Input() appVersion: any;
+  @Input() projectVersion: any;
+
+  constructor(private mapService: MapService) { }
+
+  ngOnInit(): void {
+    // Set the projectVersion class variable to an Observable that contains what was received
+    // from the version.json file. The template will then use the async pipe to subscribe to it
+    // and display the version.
+    this.projectVersion = this.mapService.getJSONData('assets/version.json');
+  }
 }
