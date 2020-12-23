b719254d1585db61dd8b5973f4d4d9c312a1cfac
Nightsphere <nightsphere88@gmail.com>
2020-12-23 22:56:57 +0000

    Issue #349: Updated legends display

--- infomapper/src/app/map-components/menu-disable.pipe.ts
+++ infomapper/src/app/map-components/menu-disable.pipe.ts
@@ -10,15 +10,15 @@ export class MenuDisablePipe implements PipeTransform {
 
   public mapLayerManager: MapLayerManager = MapLayerManager.getInstance();
 
-  transform(value: string, ...args: any[]): unknown {
-    var disableType = args[0];
+  transform(value: any, ...args: any[]): unknown {
+    var pipeType = args[0];
     var geoLayerId = args[1];
 
     // Selected Initial check.
     if (geoLayerId) {
       // If the geoLayerId is given, than the disableType is given as well, so check if it's toggleCheck for whether the
       // toggle button is toggled off or on.
-      if (disableType === 'toggleCheck') {
+      if (pipeType === 'toggleCheck') {
         if (value === undefined || value.toUpperCase() === 'TRUE') {
           return true;
         } else if (value.toUpperCase() === 'FALSE') {
@@ -53,12 +53,51 @@ export class MenuDisablePipe implements PipeTransform {
       }
       // If the disableType second argument is dataTableCheck, check the layer information from the MapLayerManager to see if it
       // is currently being displayed on the map.
-      else if (disableType === 'dataTableCheck') {
+      else if (pipeType === 'dataTableCheck') {
         let isDisplayed = this.mapLayerManager.getLayerItem(value).isDisplayedOnMainMap();
         if (isDisplayed === true) {
           return false;
         } else {
           return true;
+        }
+      }
+      // Create an InfoMapper Graduated Label to display in the legend.
+      else if (pipeType === 'IMGradLabel') {
+        
+        
+        var minInf = false;
+        var maxInf = false;
+        var valueMin = value.valueMin;
+        var valueMax = value.valueMax;
+
+        if (value.valueMin.toUpperCase().includes('-INFINITY')) {
+          valueMin = '';
+          minInf = true;
+        }
+        if (value.valueMax.toUpperCase().includes('INFINITY')) {
+          valueMax = '';
+          maxInf = true;
+        }
+
+        if (value.valueMin.includes('=')) {
+          valueMin = value.valueMin.substring(value.valueMin.indexOf('=') + 1);
+        }
+        if (value.valueMax.includes('=')) {
+          valueMax = value.valueMax.substring(value.valueMax.indexOf('=') + 1);
+        }
+
+        // if (valueMin.includes('>')) {
+        //   let temp = valueMin.split('>');
+        //   temp[0] = '>';
+        //   valueMin = temp.join(' ');
+        // }
+
+        if (minInf === true) {
+          return valueMax;
+        } else if (maxInf === true) {
+          return valueMin;
+        } else {
+          return valueMin + ' - ' + valueMax;
         }
       }
     }
